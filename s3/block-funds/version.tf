b3050a2ecfe9846f5db6341cbf0f6948c2a21001
root <root@dns.pvcb.local>
2024-07-09 11:15:16 +0700

    upload files-

--- s3/block-funds/version.tf
+++ s3/block-funds/version.tf
@@ -8,16 +8,7 @@ terraform {
     }
   }
 
-  # ##  Used for end-to-end testing on project; update to suit your needs
-  # backend "s3" {
-  #   bucket         = "ducmk254-terraform-state-s3"
-  #   region         = "ap-southeast-1"
-  #   dynamodb_table = "dynamodb-terraform-state-lock"
-  #   key            = "abcd-dev/ap-southeast-1/s3/block-funds/terraform.tfstate"
-  #   demo_access_key     = ""
-  #   demo_secret_key     = ""
-  #   role_arn       = "arn:aws:iam::413432xxxxxx:role/CrossAccountTerraformS3"
-  # }
+
 }
 
 provider "aws" {
