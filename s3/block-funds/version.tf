ecdb7a696424dfaf528773bb8edb412b61ea5832
root <root@dns.pvcb.local>
2024-07-09 11:13:24 +0700

    upload files

--- s3/block-funds/version.tf
+++ s3/block-funds/version.tf
@@ -10,7 +10,7 @@ terraform {
 
   # ##  Used for end-to-end testing on project; update to suit your needs
   # backend "s3" {
-  #   bucket         = "pvcb-terraform-state-s3"
+  #   bucket         = "ducmk254-terraform-state-s3"
   #   region         = "ap-southeast-1"
   #   dynamodb_table = "dynamodb-terraform-state-lock"
   #   key            = "abcd-dev/ap-southeast-1/s3/block-funds/terraform.tfstate"
